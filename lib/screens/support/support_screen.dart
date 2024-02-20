0f4ac0cc5e58b02609fd22af98a4febb4bb24042
himanshu17113 <himanshu17113@gmail.com>
2024-02-20 22:30:08 +0530

    suppeort fixed

--- lib/screens/support/support_screen.dart
+++ lib/screens/support/support_screen.dart
@@ -3,6 +3,7 @@ import 'package:emagz_vendor/screens/support/support_controller.dart';
 import 'package:emagz_vendor/screens/support/supportchat.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../../social_media/common/common_appbar.dart';
 
@@ -56,81 +57,73 @@ class SupportScreen extends StatelessWidget {
               padding: EdgeInsets.only(left: 16.0),
               child: Text(
                 'Recent',
-                style: TextStyle(
-                    color: textSetting,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500),
+                style: TextStyle(color: textSetting, fontSize: 14, fontWeight: FontWeight.w500),
               ),
             ),
             GetBuilder<SupportController>(
               id: "support",
               assignId: true,
               init: SupportController(),
-              builder: (supportController) => ListView.separated(
-                  scrollDirection: Axis.vertical,
-                  physics: const ScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: supportController.tickets.length,
-                  separatorBuilder: (context, index) => const Divider(),
-                  itemBuilder: (ctx, index) => ListTile(
-                        // leading: const SizedBox(
-                        //   width: 0,
-                        // ),
-                        contentPadding:
-                            const EdgeInsets.only(top: 2, left: 5, right: 4),
-                        minVerticalPadding: 0,
-                        onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SupportChat(
-                                userId:
-                                    supportController.tickets[index].userId!,
-                                conversationId:
-                                    supportController.tickets[index].id!,
-                              ),
-                            )),
-                        tileColor: Colors.white,
-                        isThreeLine: true,
-                        subtitle: Text(
-                          supportController.tickets[index].createdAt.toString(),
-                          style: const TextStyle(
-                              color: textSetting,
-                              fontSize: 10,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        trailing: const Text(
-                          'Open',
-                          style: TextStyle(
-                              color: textSetting,
-                              fontSize: 10,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Ticket No#${supportController.tickets[index].ticketNumber ?? "12345"}',
-                              style: const TextStyle(
-                                  color: textSetting,
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.w500),
+              builder: (supportController) {
+                final tickets = supportController.tickets.reversed.toList();
+                return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    physics: const ScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: tickets.length,
+                    //      separatorBuilder: (context, index) => const Divider(),
+                    itemBuilder: (ctx, index) => Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 7.5, horizontal: 12.5),
+                          child: ListTile(
+                            minLeadingWidth: 10,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                            // leading: const SizedBox(
+                            //   width: 0,
+                            // ),
+                            dense: true,
+                            contentPadding: const EdgeInsets.only(top: 5, left: 10, right: 20, bottom: 5),
+                            //minVerticalPadding: 2,
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SupportChat(
+                                    userId: tickets[index].userId!,
+                                    conversationId: tickets[index].id!,
+                                  ),
+                                )),
+                            tileColor: Colors.white,
+                            isThreeLine: true,
+                            selectedColor: chipColor,
+                            subtitle: Text(
+                              DateFormat('dd MMMM yyyy').format(tickets[index].createdAt ?? DateTime.now()),
+                              style: const TextStyle(color: textSetting, fontSize: 10, fontWeight: FontWeight.w500),
                             ),
-                            const SizedBox(
-                              height: 4,
+                            trailing: const Text(
+                              'Open',
+                              style: TextStyle(color: textSetting, fontSize: 10, fontWeight: FontWeight.w500),
                             ),
-                            Text(supportController.tickets[index].ticketTitle!),
-                          ],
-                        ),
-                      )),
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Ticket No#${tickets[index].ticketNumber ?? "12345"}',
+                                  style: const TextStyle(color: textSetting, fontSize: 10, fontWeight: FontWeight.w500),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4),
+                                  child: Text(tickets[index].ticketTitle!),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ));
+              },
             ),
             const Padding(
               padding: EdgeInsets.only(left: 16.0),
               child: Text(
                 'History',
-                style: TextStyle(
-                    color: textSetting,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500),
+                style: TextStyle(color: textSetting, fontSize: 14, fontWeight: FontWeight.w500),
               ),
             ),
             GetBuilder<SupportController>(
@@ -150,35 +143,24 @@ class SupportScreen extends StatelessWidget {
                         tileColor: Colors.white,
                         isThreeLine: true,
                         subtitle: Text(
-                          supportController.ticketsClosed[index].createdAt
-                              .toString(),
-                          style: const TextStyle(
-                              color: textSetting,
-                              fontSize: 10,
-                              fontWeight: FontWeight.w500),
+                          supportController.ticketsClosed[index].createdAt.toString(),
+                          style: const TextStyle(color: textSetting, fontSize: 10, fontWeight: FontWeight.w500),
                         ),
                         trailing: const Text(
                           'Closed',
-                          style: TextStyle(
-                              color: textSetting,
-                              fontSize: 10,
-                              fontWeight: FontWeight.w500),
+                          style: TextStyle(color: textSetting, fontSize: 10, fontWeight: FontWeight.w500),
                         ),
                         title: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
                               'Ticket No#${supportController.ticketsClosed[index].ticketNumber ?? "12345"}',
-                              style: const TextStyle(
-                                  color: textSetting,
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.w500),
+                              style: const TextStyle(color: textSetting, fontSize: 10, fontWeight: FontWeight.w500),
                             ),
                             const SizedBox(
                               height: 4,
                             ),
-                            Text(supportController
-                                .ticketsClosed[index].ticketTitle!),
+                            Text(supportController.ticketsClosed[index].ticketTitle!),
                           ],
                         ),
                       )),
@@ -194,16 +176,12 @@ class SupportScreen extends StatelessWidget {
             onPressed: () {
               showModalBottomSheet(
                 shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15))),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15))),
                 context: context,
                 builder: (contexts) => Container(
                     decoration: const BoxDecoration(
                         color: Color(0xf0E7E9FE),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(10))),
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(10))),
                     padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
                     height: s.height * .6,
                     width: s.width,
@@ -215,10 +193,8 @@ class SupportScreen extends StatelessWidget {
                             fillColor: const Color(0xffFFFFFF),
                             filled: true,
                             hintText: "What is the issue you are facing?",
-                            hintStyle: const TextStyle(
-                                fontSize: 13, color: textSetting),
-                            contentPadding: const EdgeInsets.only(
-                                left: 35, top: 15, bottom: 15),
+                            hintStyle: const TextStyle(fontSize: 13, color: textSetting),
+                            contentPadding: const EdgeInsets.only(left: 35, top: 15, bottom: 15),
                             border: OutlineInputBorder(
                                 borderSide: const BorderSide(
                                   width: 0,
@@ -233,9 +209,7 @@ class SupportScreen extends StatelessWidget {
                             initState: (_) {},
                             builder: (supportController) => ElevatedButton(
                                 onPressed: () async {
-                                 await supportController
-                                      .createTicket(tick);
-                                
+                                  await supportController.createTicket(tick);
                                 },
                                 child: const Text("Submit"))),
                       )
--- lib/social_media/common/common_appbar.dart
+++ lib/social_media/common/common_appbar.dart
@@ -1,6 +1,6 @@
 import 'package:emagz_vendor/constant/colors.dart';
 import 'package:flutter/material.dart';
- 
+
 class SocialMediaSettingAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   @override
@@ -15,46 +15,38 @@ class SocialMediaSettingAppBar extends StatelessWidget implements PreferredSizeW
   Widget build(BuildContext context) {
     return AppBar(
       automaticallyImplyLeading: false,
-      backgroundColor: Colors.transparent,
+      backgroundColor: socialBack,
       elevation: 0.0,
       // leading: null,
-
       centerTitle: false,
       title: Text(
         title,
         style: const TextStyle(color: blackButtonColor, fontSize: 22, fontWeight: FontWeight.w600),
       ),
       actions: [
-        Row(
-          children: [
-            Transform.scale(
-              scale: .7,
-              child: InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  // height: 20,
-                  // width: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: chipColor,
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 20, right: 15, top: 10, bottom: 10),
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      size: 25,
-                    ),
-                  ),
-                ),
-              ),
+        InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            alignment: Alignment.center,
+            // height: 10,
+            // width: 35,
+            margin: const EdgeInsets.only(right: 10, top: 5, bottom: 5),
+            padding: const EdgeInsets.only(
+              left: 15,
+              right: 10,
             ),
-            const SizedBox(
-              width: 10,
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: chipColor,
             ),
-          ],
+            child: const Icon(
+              Icons.arrow_back_ios,
+              //size: 20,
+            ),
+          ),
         ),
       ],
     );
